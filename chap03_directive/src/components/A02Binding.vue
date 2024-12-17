001121b11d9ca127b6aa59d92d6f297770dcf0c4
SHINSUNGJIN <pxp13716@daum.net>
2024-12-17 09:50:42 +0900

    .

--- chap03_directive/src/components/A02Binding.vue
+++ chap03_directive/src/components/A02Binding.vue
@@ -9,6 +9,11 @@ export default {
   data() {
     return {
       name: '놀부',
+      num: 10,
+      check: true,
+      arr: [10, 11],
+      user: {user: '방자', age: 20},
+      greet: '<b>Good Morning</b>',
     }
   },
   // 계산된 속성
@@ -19,7 +24,17 @@ export default {
     changeName() {
       // data, computed의 변수는 항상 this로 참조한다
       this.name = '방자';
-    }
+    },
+    onAdd(x = 0, y = 0) {
+      // console.log(this);               // 자신의 Component의 View Model
+      return `${x} + ${y} = ${x + y}`;
+    },
+    // Arrow 함수 사용 자체는 가능. 다만 this 참조(자신의 데이터)가 되지 않는다
+    onMin: (x = 0, y = 0) => {
+      // console.log(this);               // static 모드에서 window 참조는 undefined
+      return `${x} - ${y} = ${x - y}`;
+    },
+    
   }
 };
 </script>
@@ -31,29 +46,38 @@ export default {
 
   <div>
     <h5>1. 일반적 바인딩</h5>
-    일반적인 단방향 바인딩: {{ name }}<br />
-    함수의 리턴값: <br />
-    함수의 리턴값: <br />
-    배열: <br />
-    객체: <br />
+    일반적인 단방향 바인딩: {{ name }} / {{ num }}<br />
+    Boolean: {{ check ? '동의' : '동의 안함' }}<br>
+    Array: {{ arr[0] }} / {{ arr[1] }} / {{ arr[2] }}<br>
+    Object: {{ user.name }} / {{ user.age }} / {{ user.address }}<br>
+
+    함수의 리턴값: {{ onAdd(10, 20) }}<br />
+    함수의 리턴값: {{ onMin(10, 20) }} <br />
   </div>
   <br />
 
   <div>
-    <h5>2. 바인딩 연산</h5>
-    일반적 연산: <br />
-    속성 참조: <br />
-    속성 참조 연산: <br />
-    비교 연산: <br />
-    비교 연산: <br />
-    삼항 연산: <br />
+    <h5>2. 바인딩 연산 - <span v-pre>{{ => 자바스크립트의 시작, }} => 자바스크립트 종료</span></h5>
+    <!-- 바인딩 내부에서는 식 사용은 가능하다 -->
+    일반적 연산: {{ 10 + 20 }}<br />
+    속성 참조: {{ arr.length }}<br />
+    속성 참조 연산: {{ arr.length * 100 }} <br />
+    비교 연산: {{ arr[0] > 0 }} <br />
+    비교 연산: {{ arr[0] > 0 && arr[1] > 0 }}<br />
+    삼항 연산: {{ name === '놀부' ? '관리자' : '일반 유저' }}<br />
   </div>
   <br />
 
   <div>
+    <!-- 
+      vue가 제공해주는 지시자(Directive)는 "v-"로 시작한다
+      지시자의 값은 항상 변수명 또는 실질적인 값을 지정해야 한다(문자열이 아님) 
+    -->
     <h5>3. 바인딩 관련 지시자</h5>
-    v-text: <span></span><br />
-    v-html: <span></span><br />
+    바인딩: {{ greet }}<br>
+    v-text: <span v-text="'Hello World'"></span><br />
+    v-text: <span v-text="greet"></span><br />
+    v-html: <span v-html="greet"></span><br />
     v-once: <span></span><br />
     v-pre: <span></span><br />
     <br />
